31b9ed3005ef775086b13317e79163a93cfc648a
tush-tr <tusharrajput604@gmail.com>
2021-11-15 00:35:32 +0530

    fix: corrected config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "master",
+    branches: "main",
     repositoryUrl: "https://github.com/tush-tr/github-actions-ci-workflow",
     plugins: [
         "@semantic-release/commit-analyzer",
