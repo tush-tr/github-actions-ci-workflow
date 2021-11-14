08634b3dedb6ebd057068d214e46ce05523802d8
tush-tr <tusharrajput604@gmail.com>
2021-11-14 23:45:54 +0530

    corrected format, added semantic versioning

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/tush-tr/github-actions-ci-workflow",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
-}
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+    ],
+};
