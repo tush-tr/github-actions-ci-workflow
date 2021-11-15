c82ae7eff0599b1d394693dcc3c03f58e6366666
tush-tr <tusharrajput604@gmail.com>
2021-11-16 01:26:07 +0530

    build: new action for opening issues

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core')
+const gituhb = require('@actions/gitub')
+try{
+    const title = core.getInput('title')
+    const body = core.getInput('body')
+    const assignees = core.getInput('assignees')
+    const token = core.getInput('token')
+    const octokit = new github.Github(token)
+    
+    const response = octokit.rest.issues.create({
+        ...github.context.repo, // owner , repo
+        title,
+        body,
+        assignees: assignees ? assignees.split('\n') : undefined
+      });
+    
+    core.setOutput('issue', JSON.stringify(response.data))
+
+} catch(err){
+    core.setFailed(error.message)
+}
