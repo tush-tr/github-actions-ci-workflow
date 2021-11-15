d2da996f006e77f28143430d0c08e08516562c7e
tush-tr <tusharrajput604@gmail.com>
2021-11-16 00:07:21 +0530

    fix: recompiled index.js

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -8492,13 +8492,19 @@ const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 try {
     // throw (new Error('Some error message'))
-    const name = core.getInput('who-to-greet')
+    core.debug('Debug message')
+    core.warning('Warning message')
+    core.error('error message')
 
+    const name = core.getInput('who-to-greet')
+    core.setSecret('dwdw');
     console.log(`Hello ${name}`)
     const time = new Date();
     core.setOutput("time", time.toTimeString())
-
+    core.startGroup('Logging github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup();
+    core.exportVariable('HELLO',"hello")
 } catch (error){
     core.setFailed(error.message);
 }
