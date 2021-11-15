24ad860a9fb0fbcae2bad9057f2cbc2e725dd317
tush-tr <tusharrajput604@gmail.com>
2021-11-16 00:06:46 +0530

    fix: adding more core package

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,13 +2,19 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 try {
     // throw (new Error('Some error message'))
-    const name = core.getInput('who-to-greet')
+    core.debug('Debug message')
+    core.warning('Warning message')
+    core.error('error message')
 
+    const name = core.getInput('who-to-greet')
+    core.setSecret('dwdw');
     console.log(`Hello ${name}`)
     const time = new Date();
     core.setOutput("time", time.toTimeString())
-
+    core.startGroup('Logging github object')
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup();
+    core.exportVariable('HELLO',"hello")
 } catch (error){
     core.setFailed(error.message);
 }
